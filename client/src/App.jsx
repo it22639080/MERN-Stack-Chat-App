a995129de86e4a38ad54532220c79c9b614c6230
Kavindu Madhusanka <127321810+it22639080@users.noreply.github.com>
2025-07-13 23:41:19 +0530

    secound

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const {authUser} = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-no-repeat bg-center h-screen w-screen">
+    <div className="bg-[url('/bgImage.svg')] bg-cover bg-no-repeat bg-center h-screen w-screen">
       <Toaster/>
     <Routes>
       <Route path="/" element={authUser ? <HomePage/> : <Navigate to ="/login"/>} />
--- server/server.js
+++ server/server.js
@@ -50,8 +50,11 @@ app.use("/api/messages",messageRouter);
 
 
 await connectDB();
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+if(process.env.NODE_ENV !== "production "){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+}
+export default server;
+
